f516dbf013d073e7ee2bd34958d6aa3d43871397
zoltan-mihalyi <zoltan.mihalyi.90@gmail.com>
2015-11-09 08:29:26 +0100

    Event annotation is public

--- mp-client/src/main/java/hu/zoltanmihalyi/mp/Event.java
+++ mp-client/src/main/java/hu/zoltanmihalyi/mp/Event.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.METHOD)
-@interface Event {
+public @interface Event {
     EventType type();
 
     String roomName();
